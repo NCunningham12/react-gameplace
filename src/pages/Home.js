6a0cb448905793262027e034441e662b792e94a3
Nathan Cunningham <counterweight22@gmail.com>
2023-03-25 02:37:15 -0700

    Carousel HTML and CSS done. (Needs JS still)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Carousel from '../components/Carousel';
 
 function Home() {
   return (
-    <div>Home Page Content</div>
-  )
+    <>
+      <Carousel />
+    </>
+  );
 }
 
-export default Home
+export default Home;
